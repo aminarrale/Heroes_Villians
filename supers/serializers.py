0045260c01be96173e7bd51d3be8b9d32acc6700
Karim <aarrale12@gmail.com>
2022-06-30 17:29:34 -0500

    Urls and views files are being modified

--- supers/serializers.py
+++ supers/serializers.py
@@ -6,3 +6,7 @@ class SuperSerializer(serializers.ModelSerializer):
         model = Super
         fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
         depth = 1
+
+    
+
+    
